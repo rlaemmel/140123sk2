afcf694f7e2539c7379be116d1f15ef3d20b4a78
Ralf Laemmel <rlaemmel@gmail.com>
2014-01-28 01:54:52 +0100

    Improved Haskell code

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -1,27 +1,29 @@
+import Data.List (sortBy)
+
 -- Valid digits for hh (in 24h)
-hour = [ (x, y) | x <- [0..2], y <- [0..9], f x y ]
+hh = [ (x, y) | x <- [0..2], y <- [0..9], f x y ]
   where f x y = x /= 2 || y < 4
 
 -- Valid digits for mm
-minute = [ (x, y) | x <- [0..5], y <- [0..9] ]
+mm = [ (x, y) | x <- [0..5], y <- [0..9] ]
 
--- Minute is the palindrom of the hour
-palindrom = [ (x, y, m) |
-  (x, y) <- hour,
-  elem (y, x) minute,
-  let m = (x*10+y)*60+(y*10)+x ]
+-- Subset of hh with palindrom in mm
+hh' = [ (x, y, f x y) | (x, y) <- hh, elem (y, x) mm ]
+  where
+    -- Add absolute minute during day
+    f x y = (x*10+y)*60+(y*10)+x
 
--- Find sibling palindroms with max distance
-max1 = max2 palindrom
+-- Pairs of sibling palindroms with minute difference
+siblings = map f (zip hh' (tail hh' ++ [head hh']))
+  where
+    f ((x, y, a), (x', y', a')) = (((x, y), (x', y')), d)
+      where
+        d = (if a' > a then a' else (a'+24*60))-a
 
--- Initialize search with first 2 palindroms 
-max2 ((x1, y1, m1):l'@((x2, y2, m2):l)) =
-  max3 (m2-m1, False, x1, y1, x2, y2) l'
+-- Siblings sorted by minute difference
+siblings' = sortBy f siblings
+  where
+    f x y = compare (snd x) (snd y)
 
--- Keep on searching through the list
-max3 z [_] = z
-max3 (d1, tf, x1, y1, x2, y2) ((x3, y3, m3):l'@((x4, y4, m4):l)) =
-  let d2 = m4 - m3 in
-    if (d2 > d1) then max3 (d2, False, x3, y3, x4, y4) l'
-    else if (d2 == d1) then max3 (d2, True, x3, y3, x4, y4) l'
-    else max3 (d1, tf, x1, y1, x2, y2) l'
+-- Sibling with maximum minute difference
+maxSibling = last siblings'
